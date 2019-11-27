698f69c62a460ea3773f14e32b66b1b705250b13
0dminnimda <52697657+0dminnimda@users.noreply.github.com>
2019-11-28 00:57:50 +0300

    ю

--- physical_simulation.py
+++ physical_simulation.py
@@ -1,6 +1,3 @@
-import os
-
-
 def forse(r):
     global g,m1,m2
     fo = g*m1*m2/r**2
@@ -29,15 +26,15 @@ def coun(pos1,pos2,vel1,vel2,step):
     return pos1, pos2, vel1, vel2, a1, a2
 
 
-m1=1*10**-3
-m2=2*10**-3
+m1=1*10**-0
+m2=2*10**-0
 #m1 = m1**-1
 #m2 = m2**-1
 g=6.6743015*10**-11
 r = 1*10**-0
 pos1, pos2=0, r
 vel1, vel2=0, 0
-step=1*10**0.5
+step=1*10**-1
 num1 = 0
 arr_v1 = []
 arr_v2 = []
@@ -46,10 +43,10 @@ arr_p2 = []
 b = False
 while 1:
     #if num1 % 500 == 0 and b is True:
-    arr_v1.append(vel1)
-    arr_v2.append(vel2)
-    arr_p1.append(pos1)
-    arr_p2.append(pos2)
+    #arr_v1.append(vel1)
+    #arr_v2.append(vel2)
+    #arr_p1.append(pos1)
+    #arr_p2.append(pos2)
     num1 += 1
     t = coun(pos1,pos2,vel1,vel2,step)
     pos1, pos2, vel1, vel2, a1, a2 = t
@@ -72,16 +69,16 @@ print(" скорость 1 тела при столкнлвении",vel1)
 print(" скорость 2 тела при столкнлвении",vel2,"\n")
 #print(arr_v,arr_p)
 
-f1 = open("text1.txt", "w")
-f2 = open("text2.txt", "w")
-f3 = open("text3.txt", "w")
-f4 = open("text4.txt", "w")
-for i in arr_p1:
-    f1.write(str(i)+" ")
-    f2.write(str(i)+" ")
-    f3.write(str(i)+" ")
-    f4.write(str(i)+" ")
-f1.close()
-f2.close()
-f3.close()
-f4.close()
+#f1 = open("text1.txt", "w")
+#f2 = open("text2.txt", "w")
+#f3 = open("text3.txt", "w")
+#f4 = open("text4.txt", "w")
+#for i in arr_p1:
+#    f1.write(str(i)+" ")
+#    f2.write(str(i)+" ")
+#    f3.write(str(i)+" ")
+#    f4.write(str(i)+" ")
+#f1.close()
+#f2.close()
+#f3.close()
+#f4.close()
--- модуль1.py
+++ модуль1.py
@@ -7,7 +7,7 @@ f = open("text1.txt", "r")
 t = f.read()
 arr1 = list(t.split())
 for i in range(len(arr1)):
-    if i % 10**3 == 0:
+    if i % 10**4 == 0:
         pos1.append(float(arr1[i]))
 else:
     pos2.append(float(arr1[-1]))
@@ -17,13 +17,13 @@ f = open("text2.txt", "r")
 t = f.read()
 arr2 = list(t.split())
 for i in range(len(arr2)):
-    if i % 10**3 == 0:
+    if i % 10**4 == 0:
         pos2.append(float(arr2[i]))
 else:
     pos2.append(float(arr2[-1]))
 f.close()
 print(len(arr1),len(pos1))
-co = 10**3.10075
+co = 10**2.1008
 img2 = np.zeros((700//2,500,3))
 #print(img.shape[2]-img.shape[1]//6-int(pos2[2]*co))
 
