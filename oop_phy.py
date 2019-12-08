fe8efc66d8b423fcc36db1750ff68ea75417e984
0dminnimda <52697657+0dminnimda@users.noreply.github.com>
2019-12-08 20:14:56 +0300

    Update oop_phy.py

--- oop_phy.py
+++ oop_phy.py
@@ -8,7 +8,7 @@ class body():
         self.rad = 2
         self.x, self.y = pos
         self.xv, self.yv = vel
-        self.v_vec = v_vec
+        self.v_vec = [self.x, self.y, self.xv, self.yv]#v_vec
         self.m = m*10**-5
         #self.g=6.6743015*10**-11
         self.step=step
@@ -22,9 +22,17 @@ class body():
         else:
             print(self.__dict__, end=en)
 
-    def vec(self, ob):
+    def m_vec(self, ob):
+        def add_vectors(v, w):
+            return [vi + wi for vi, wi in zip(v, w)]
+
+        def sum_of_all_vectors(vecs):
+            return reduce(add_vectors, vecs)
+
         def ve_l(a):
-            return np.linalg.norm([a[2]-a[0], a[3]-a[1]])
+            b = np.linalg.norm([a[2]-a[0], a[3]-a[1]])
+            print(a,b)
+            return b
 
         def v_vec(r, m, step):
             r[2] *= step*ve_l(r)**3/m
@@ -48,10 +56,9 @@ class body():
             return vec
 
         vec = self.v_vec
-        r = [self.x, self.y, ob.x, ob.y]
+        r = [vec[0], vec[1], ob.v_vec[0], ob.v_vec[1]]
         vec = new_vec(vec, r, ob.m, self.step)
         self.v_vec = move(vec)
-
 
     def main(self, ob):
         dm = ob.m
@@ -105,8 +112,8 @@ class body():
 
 
 step=1*10**0
-tt = [body(1, (10*10**0, 0), (0,-1*10**-3), step),
-     body(1, (0*10**0, 0), (0,0*10**-3.5), step)]
+tt = [body(1, (10*10**0, 0), (0,-1*10**-1), [], step),
+     body(1,  (0*10**0, 0), (0,0*10**-3.5), [], step)]
 a = tt[0]
 b = tt[1]
 a.pr()
@@ -117,15 +124,16 @@ scax = scay = 10
 co = 0
 path = np.zeros((720, 1000, 3))
 while 1:
-    a.main(b)
-    b.main(a)
+    a.m_vec(b)
+    b.m_vec(a)
+
     if co%100 == 0:
         path = a.draw(path, (0,0,255), 1, scax, scay)
         path = b.draw(path, (255,0,0), 1, scax, scay)
     
     if co%1000 == 0:
-        img = a.draw(path.copy(), (0,0,255), 7, scax, scay)
-        img = b.draw(img, (255,0,0), 7, scax, scay)
+        img = a.draw(path.copy(), (0,0,255), 3, scax, scay)
+        img = b.draw(img, (255,0,0), 3, scax, scay)
         cv.imshow("img", img)
         if cv.waitKey(1) & 0xFF == ord('2'):
             cv.destroyAllWindows()
