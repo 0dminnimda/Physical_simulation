facd21d4eb65c46feb2e71a8847df2d35c908d71
0dminnimda <52697657+0dminnimda@users.noreply.github.com>
2019-12-08 15:17:43 +0300

    Update oop_phy.py

--- oop_phy.py
+++ oop_phy.py
@@ -62,15 +62,11 @@ class body():
         #return mx, my, bo
         pass
 
-    def draw(self, co, path, col, mul, mul2):
+    def draw(self, co, path, col, r, mul, mul2):
         px, py = self.x, self.y
         #if co%10 == 0:
         hx, hy = path.shape[1]/2 + px*mul, path.shape[0]/2 + py*mul2
-        cv.circle(path, (int(hx), int(hy)), 1, col, -1)
-
-        #if co%100 == 0:
-        #    hx2, hy2 = path.shape[1]/2 + px*mul, path.shape[0]/2 + py*mul2
-        #    cv.circle(img, (int(hx2), int(hy2)), 10, col, -1)
+        cv.circle(path, (int(hx), int(hy)), r, col, -1)
 
         return path
 
@@ -91,32 +87,16 @@ while 1:
     b.main(a)
     #img = path.copy()
     if co%10 == 0:
-        path = a.draw(co, path, (0,0,255), mul, mul2)
-        path = b.draw(co, path, (255,0,0), mul, mul2)
+        path = a.draw(co, path, (0,0,255), 1, mul, mul2)
+        path = b.draw(co, path, (255,0,0), 1, mul, mul2)
     
     if co%1000 == 0:
-        #print(path, img)
-        #print(a.pr(),b.pr())
-        cv.imshow("img",path)
+        img = a.draw(co, path.copy(), (0,0,255), 7, mul, mul2)
+        img = b.draw(co, img, (255,0,0), 7, mul, mul2)
+        cv.imshow("img", img)
         if cv.waitKey(1) & 0xFF == ord('2'):
             cv.destroyAllWindows()
             break
-
-    #print(som,so2)
-    #if co%1 == 0:
-    #    cv.circle(path, (int(som[0]*mul)+path.shape[0]//2, int(som[1]*mul)+path.shape[1]//2), 1, (255,0,0), -1)
-    #    cv.circle(path, (int(som[0]*mul)+path.shape[0]//2, int(som[1]*mul)+path.shape[1]//2), 1, (0,255,0), -1)
-    #if co%100 == 0:
-    #    img = path.copy()
-    #    cv.circle(img, (int(som[0]*mul)+path.shape[0]//2, int(som[1]*mul)+path.shape[1]//2), 7, (255,0,0), -1)  # int(a.m*mul2)
-    #    cv.circle(img, (int(som[0]*mul)+path.shape[0]//2, int(som[1]*mul)+path.shape[1]//2), 7, (0,255,0), -1)
-    #    cv.imshow("img",img)
-    #    if cv.waitKey(1) & 0xFF == ord('2'):
-    #        cv.destroyAllWindows()
-    #        break
-    #if som[-1] is True or so2[-1] is True:
-        #print(som[0],so2[0])
-        #break
     co += 1
 #print(co*a.step)
 #print(time.time()-st)
