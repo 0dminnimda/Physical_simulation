c3368b644691b1919173425da7459a777f9bcb28
0dminnimda <52697657+0dminnimda@users.noreply.github.com>
2019-12-09 22:37:18 +0300

    Update oop_phy.py

--- oop_phy.py
+++ oop_phy.py
@@ -2,6 +2,7 @@ import numpy as np
 import time
 import cv2 as cv
 from functools import reduce
+import random as ra
 
 
 def add_vec(v, w):
@@ -38,12 +39,15 @@ class body():
         else:
             print(self.__dict__, end=en)
 
-    def calc(self, ob):
-
-        mx, my = self.x, self.y
-        dx, dy = ob.x, ob.y
-        nvec = v_vec([-mx+dx, -my+dy], ob.m, self.step)
-        self.vec = sum_vec(nvec, self.vec)
+    def calc(self, *obb):
+        for ob in obb:
+            mx, my = self.x, self.y
+            dx, dy = ob.x, ob.y
+            nvec = v_vec([-mx+dx, -my+dy], ob.m, self.step)
+            self.vec = sum_vec(nvec, self.vec)
+        vec = self.vec
+        self.x += vec[0]
+        self.y += vec[1]
 
     def move(self):
         vec = self.vec
@@ -101,40 +105,49 @@ class body():
         return path
 
 
-step=1*10**-6
+step=1*10**-7
 
-xp1, yp1 = 0,0
-xp2, yp2 = -10, 0
+xp1, yp1 = 5, -3
+xp2, yp2 = -5, -3
+xp3, yp3 = 0, 4.5
 
-xv1, yv1 = -0*10**-4, 1*10**-3.5
-xv2, yv2 = 1*10**-4, 0
+xv1, yv1 = ra.randint(-2,2)*10**-4, ra.randint(-2,2)*10**-4
+xv2, yv2 = ra.randint(-2,2)*10**-4, ra.randint(-2,2)*10**-4
+xv3, yv3 = ra.randint(-2,2)*10**-4, ra.randint(-2,2)*10**-4
 
-m1 = 1
-m2 = 1
+m1 = 3
+m2 = 3
+m3 = 3
 
 a = body(m1, [xp1, yp1], [xv1, yv1], step)
 b = body(m2, [xp2, yp2], [xv2, yv2], step)
+c = body(m3, [xp3, yp3], [xv3, yv3], step)
 
 scax = scay = 10
-indx, indy = 0, -40  # percent
+indx, indy = 0, 0  # percent
 co = 0
 path = np.zeros((790, 1300, 3))
 while 1:
-    a.calc(b)
-    a.move()
-    b.calc(a)
-    b.move()
+    a.calc(b, c)
+    #a.move()
+    b.calc(a, c)
+    #b.move()
+    c.calc(a, b)
+    #c.move()
 
-    if co%1 == 0:
+    if co%100 == 0:
         path = a.draw(path, (0,0,255), 1, scax, scay, indx, indy)
         path = b.draw(path, (255,0,0), 1, scax, scay, indx, indy)
+        path = c.draw(path, (0,255,0), 1, scax, scay, indx, indy)
     
     if co%1000 == 0:
         img = a.draw(path.copy(), (0,0,255), 6, scax, scay, indx, indy)
         img = b.draw(img, (255,0,0), 6, scax, scay, indx, indy)
+        img = c.draw(img, (0,255,0), 6, scax, scay, indx, indy)
         cv.imshow("img", img)
         if cv.waitKey(1) & 0xFF == ord('2'):
-            cv.imwrite("physics_sim.png", img)
+            #cv.imwrite("physics_sim.png", img)
             cv.destroyAllWindows()
             break
     co += 1
+print(co)
